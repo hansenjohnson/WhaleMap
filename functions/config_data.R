cc46a5f1a621e0cdc4c251b2a01b3622f24b6331
Hansen Johnson <hansen.johnson@dal.ca>
2017-10-04 15:31:02 -0300

    add name check

--- functions/config_data.R
+++ functions/config_data.R
@@ -1,12 +1,31 @@
 config_tracks = function(tracks){
   
+  # list required column names
+  columns = c('time',
+              'lat',
+              'lon',
+              'date', 
+              'yday',
+              'year',
+              'platform',
+              'name',
+              'id')
+  
+  # test column names
+  test = columns %in% colnames(tracks)
+  
+  if(FALSE %in% test){
+    ind = which(!columns %in% colnames(tracks))
+    stop('No column(s) called ', paste(as.character(columns[ind]), collapse = " or ") , '!')
+  }
+  
   # configure column types
   tracks$time = as.POSIXct(tracks$time, tz = 'UTC', usetz = T)
   tracks$lat = as.numeric(tracks$lat)
   tracks$lon = as.numeric(tracks$lon)
   tracks$date = as.Date(tracks$date)
   tracks$yday = as.numeric(tracks$yday)
-  tracks$year = as.character(tracks$year)
+  tracks$year = as.numeric(tracks$year)
   tracks$platform = as.factor(tracks$platform)
   tracks$name = as.factor(tracks$name)
   tracks$id = as.character(tracks$id)
@@ -16,6 +35,28 @@ config_tracks = function(tracks){
 
 config_observations = function(obs){
   
+  # list required column names
+  columns = c('time',
+              'lat',
+              'lon',
+              'date', 
+              'yday',
+              'species',
+              'score',
+              'number',
+              'year',
+              'platform',
+              'name',
+              'id')
+  
+  # test column names
+  test = columns %in% colnames(obs)
+  
+  if(FALSE %in% test){
+    ind = which(!columns %in% colnames(obs))
+    stop('No column(s) called ', paste(as.character(columns[ind]), collapse = " or ") , '!')
+  }
+  
   # configure column types
   obs$time = as.POSIXct(obs$time, tz = 'UTC', usetz = T)
   obs$species = as.factor(obs$species)
@@ -23,7 +64,7 @@ config_observations = function(obs){
   obs$lon = as.numeric(obs$lon)
   obs$date = as.Date(obs$date)
   obs$yday = as.numeric(obs$yday)
-  obs$year = as.character(obs$year)
+  obs$year = as.numeric(obs$year)
   obs$platform = as.factor(obs$platform)
   obs$name = as.factor(obs$name)
   obs$id = as.character(obs$id)
