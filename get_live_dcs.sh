00d1ac0112e1685197ebceb0f6d37088a5c4349b
Hansen Johnson <hansen.johnson@dal.ca>
2017-10-05 22:33:05 -0300

    include dal556 mission in GSL

--- get_live_dcs.sh
+++ get_live_dcs.sh
@@ -11,7 +11,7 @@ declare -A URL
 # assign paths to detection data for each deployment
 URL=(
 	# live
-	#[2017-10-03_slocum_dal556]=http://dcs.whoi.edu/dal1017/dal1017_dal556_html/ptracks/manual_analysis.csv
+	[2017-10-03_slocum_dal556]=http://dcs.whoi.edu/dal1017/dal1017_dal556_html/ptracks/manual_analysis.csv
 	[2017-08-02_slocum_otn200]=http://dcs.whoi.edu/dal0817/dal0817_otn200_html/ptracks/manual_analysis.csv
 	[2017-06-23_wave_crispusattucks]=http://dcs.whoi.edu/gom0717/gom0717_crispusattucks_html/ptracks/manual_analysis.csv
 	[2016-09-28_buoy_nomans]=http://dcs.whoi.edu/nomans0916/dmon028_html/ptracks/manual_analysis.csv
