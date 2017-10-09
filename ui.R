922cc84f3d02d7d25cd48b3e87ef01ac47c536b4
Hansen Johnson <hansen.johnson@dal.ca>
2017-10-09 17:52:22 -0300

    reveal update glider data button for debugging

--- ui.R
+++ ui.R
@@ -128,10 +128,10 @@ body <- dashboardBody(
                actionButton("zoom", "Center map"),
                
                helpText('Please be patient if viewing data from many years. 
-                                       Consider turning tracks \'off\' below to speed up plotting.')
+                                       Consider turning tracks \'off\' below to speed up plotting.'),
 
-               # # update live glider
-               # actionButton("update", label = 'Update glider data')
+               # update live glider
+               actionButton("update", label = 'Update glider data')
                
            ),
            
