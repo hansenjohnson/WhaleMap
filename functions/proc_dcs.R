7f99d7f66fcf24bdf7c598558ca4836a5a631921
Hansen Johnson <hansen.johnson@dal.ca>
2017-10-10 08:32:27 -0300

    convert proc_dcs to function that's run over live and archived data

--- functions/proc_dcs.R
+++ functions/proc_dcs.R
@@ -1,164 +1,159 @@
-# process and save all live and archived dcs (glider, buoy, etc) deployment tracklines and detections
+# dcs processing function
 
-# user input --------------------------------------------------------------
+proc_dcs = function(data_dir, output_dir, det_fname, track_fname, ext = ""){
 
-# directory to look for files
-data_dir = 'data/raw/dcs/'
-
-# directory for output
-output_dir = 'data/interim/'
-
-# setup -------------------------------------------------------------------
-
-# libraries
-library(lubridate)
-library(sp)
-library(reshape2)
-source('functions/config_data.R')
-
-# define functions --------------------------------------------------------
-
-insert_NAs = function(d, thresh = 2*60*60){
-  # insert an NA into the data frame when the time between observations exceeds a threshold
+  # setup -------------------------------------------------------------------
   
-  # create time vector
-  t = as.POSIXct(d$time, tz = 'UTC')
+  # libraries
+  library(lubridate)
+  library(sp)
+  library(reshape2)
+  source('functions/config_data.R')
   
-  # align times to calculate difference
-  t0 = t[1:length(t)-1]
-  t1 = t[2:length(t)]
+  # define functions --------------------------------------------------------
   
-  # find rows where the time difference exceeds the threshold
-  ind = which(abs(difftime(t0, t1))>thresh)
-  
-  # return if threshold is not exceeded
-  if(length(ind)==0){return(d)}
-  
-  # define rows of NAs to insert
-  NArow = d[ind,]
-  NArow$lat = NA
-  NArow$lon = NA
-  #NArow$time = as.POSIXct(NArow$time) + thresh
-  
-  # loop to make NA insertions
-  c = 0 # initialize counter
-  for(i in 1:length(ind)){
-    # find index, correcting for growing data frame because of other insertions
-    id = ind[i]+c 
+  insert_NAs = function(d, thresh = 2*60*60){
+    # insert an NA into the data frame when the time between observations exceeds a threshold
     
-    # insert row
-    d = rbind(d[1:id,],NArow[i,],d[-(1:id),])  
+    # create time vector
+    t = as.POSIXct(d$time, tz = 'UTC')
     
-    # add to counter
-    c = c+1
+    # align times to calculate difference
+    t0 = t[1:length(t)-1]
+    t1 = t[2:length(t)]
+    
+    # find rows where the time difference exceeds the threshold
+    ind = which(abs(difftime(t0, t1))>thresh)
+    
+    # return if threshold is not exceeded
+    if(length(ind)==0){return(d)}
+    
+    # define rows of NAs to insert
+    NArow = d[ind,]
+    NArow$lat = NA
+    NArow$lon = NA
+    #NArow$time = as.POSIXct(NArow$time) + thresh
+    
+    # loop to make NA insertions
+    c = 0 # initialize counter
+    for(i in 1:length(ind)){
+      # find index, correcting for growing data frame because of other insertions
+      id = ind[i]+c 
+      
+      # insert row
+      d = rbind(d[1:id,],NArow[i,],d[-(1:id),])  
+      
+      # add to counter
+      c = c+1
+    }
+    
+    return(d)
   }
   
-  return(d)
+  # process data ------------------------------------------------------------
+  
+  # list detection files
+  detection_dir_list = list.files(path = data_dir, full.names = T)
+  
+  detection_list = list()
+  for(i in seq_along(detection_dir_list)){
+    
+    # determine deployment directory
+    dir = detection_dir_list[[i]]
+    
+    # read in detection data
+    d = read.csv(paste0(dir,'/manual_analysis.csv'))
+    
+    # remove unneeded columns
+    d$analyst = NULL
+    d$notes = NULL
+    
+    # fix time
+    d$datetime_utc = as.character(d$datetime_utc)
+    d$time = as.POSIXct(d$datetime_utc,format = '%Y%m%d%H%M%S',tz = 'UTC')
+    d$datetime_utc = NULL
+    
+    # configure time
+    d$time = format(d$time, tz="UTC",usetz=TRUE)
+    d$date = as.Date(d$time)
+    d$yday = yday(d$time)
+    d$year = year(d$time)
+    
+    # add deployment metadata
+    d$id = paste0(basename(dir), ext)
+    d$platform = strsplit(dir, '_')[[1]][2]
+    d$name = strsplit(dir, '_')[[1]][3]
+    
+    # insert NAs when time threshold is exceeded
+    d = insert_NAs(d)
+    
+    # add to the list
+    detection_list[[i]] = d
+  }
+  
+  # flatten list
+  all = do.call(rbind, detection_list)
+  
+  # convert column types
+  all$platform = as.factor(all$platform)
+  all$name = as.factor(all$name)
+  all$time = as.POSIXct(all$time, tz="UTC")
+  
+  # convert score names
+  # sei
+  all$sei = gsub(all$sei, pattern = 'present', replacement = 'detected')
+  all$sei = gsub(all$sei, pattern = 'maybe', replacement = 'possibly detected')
+  all$sei = gsub(all$sei, pattern = 'absent', replacement = 'not detected')
+  all$sei = as.factor(all$sei)
+  
+  # fin
+  all$fin = gsub(all$fin, pattern = 'present', replacement = 'detected')
+  all$fin = gsub(all$fin, pattern = 'maybe', replacement = 'possibly detected')
+  all$fin = gsub(all$fin, pattern = 'absent', replacement = 'not detected')
+  all$fin = as.factor(all$fin)
+  
+  # right
+  all$right = gsub(all$right, pattern = 'present', replacement = 'detected')
+  all$right = gsub(all$right, pattern = 'maybe', replacement = 'possibly detected')
+  all$right = gsub(all$right, pattern = 'absent', replacement = 'not detected')
+  all$right = as.factor(all$right)
+  
+  # humpback
+  all$humpback = gsub(all$humpback, pattern = 'present', replacement = 'detected')
+  all$humpback = gsub(all$humpback, pattern = 'maybe', replacement = 'possibly detected')
+  all$humpback = gsub(all$humpback, pattern = 'absent', replacement = 'not detected')
+  all$humpback = as.factor(all$humpback)
+  
+  # drop all unused levels
+  all = droplevels(all)
+  
+  # create trackline file ---------------------------------------------------
+  
+  # remove species info and keep tracklines
+  tracks = all[,-c(1:4)]
+  
+  # config data types
+  tracks = config_tracks(tracks)
+  
+  # save output
+  saveRDS(tracks, paste0(output_dir, track_fname))
+  
+  # create detections file --------------------------------------------------
+  
+  # convert detections to long form
+  detections = melt(all, measure.vars = c('right', 'sei', 'fin', 'humpback'), variable.name = 'species', value.name = 'score')
+  
+  # remove absences to reduce data frame size
+  detections = detections[detections$score!='not detected',]
+  
+  # add number column
+  detections$number = NA
+  
+  # config data types
+  detections = config_observations(detections)
+  
+  # save output
+  saveRDS(detections, paste0(output_dir, det_fname))
+  
 }
 
-# process data ------------------------------------------------------------
-
-# list detection files (live and archived)
-detection_dir_list = c(list.files(path = paste0(data_dir, 'archived'), full.names = T),
-                       list.files(path = paste0(data_dir, 'live'), full.names = T))
-
-detection_list = list()
-for(i in seq_along(detection_dir_list)){
-  
-  # determine deployment directory
-  dir = detection_dir_list[[i]]
-  
-  # read in detection data
-  d = read.csv(paste0(dir,'/manual_analysis.csv'))
-  
-  # remove unneeded columns
-  d$analyst = NULL
-  d$notes = NULL
-  
-  # fix time
-  d$datetime_utc = as.character(d$datetime_utc)
-  d$time = as.POSIXct(d$datetime_utc,format = '%Y%m%d%H%M%S',tz = 'UTC')
-  d$datetime_utc = NULL
-  
-  # configure time
-  d$time = format(d$time, tz="UTC",usetz=TRUE)
-  d$date = as.Date(d$time)
-  d$yday = yday(d$time)
-  d$year = year(d$time)
-  
-  # add deployment metadata
-  d$id = basename(dir)
-  # d$start_date = basename(strsplit(dir, '_')[[1]][1])
-  d$platform = strsplit(dir, '_')[[1]][2]
-  d$name = strsplit(dir, '_')[[1]][3]
-  
-  # insert NAs when time threshold is exceeded
-  d = insert_NAs(d)
-  
-  # add to the list
-  detection_list[[i]] = d
-}
-
-# flatten list
-all = do.call(rbind, detection_list)
-
-# convert column types
-all$platform = as.factor(all$platform)
-all$name = as.factor(all$name)
-all$time = as.POSIXct(all$time, tz="UTC")
-
-# convert score names
-# sei
-all$sei = gsub(all$sei, pattern = 'present', replacement = 'detected')
-all$sei = gsub(all$sei, pattern = 'maybe', replacement = 'possibly detected')
-all$sei = gsub(all$sei, pattern = 'absent', replacement = 'not detected')
-all$sei = as.factor(all$sei)
-
-# fin
-all$fin = gsub(all$fin, pattern = 'present', replacement = 'detected')
-all$fin = gsub(all$fin, pattern = 'maybe', replacement = 'possibly detected')
-all$fin = gsub(all$fin, pattern = 'absent', replacement = 'not detected')
-all$fin = as.factor(all$fin)
-
-# right
-all$right = gsub(all$right, pattern = 'present', replacement = 'detected')
-all$right = gsub(all$right, pattern = 'maybe', replacement = 'possibly detected')
-all$right = gsub(all$right, pattern = 'absent', replacement = 'not detected')
-all$right = as.factor(all$right)
-
-# humpback
-all$humpback = gsub(all$humpback, pattern = 'present', replacement = 'detected')
-all$humpback = gsub(all$humpback, pattern = 'maybe', replacement = 'possibly detected')
-all$humpback = gsub(all$humpback, pattern = 'absent', replacement = 'not detected')
-all$humpback = as.factor(all$humpback)
-
-# drop all unused levels
-all = droplevels(all)
-
-# create trackline file ---------------------------------------------------
-
-# remove species info and keep tracklines
-tracks = all[,-c(1:4)]
-
-# config data types
-tracks = config_tracks(tracks)
-
-# save output
-saveRDS(tracks, paste0(output_dir, 'dcs_tracks.rds'))
-
-# create detections file --------------------------------------------------
-
-# convert detections to long form
-detections = melt(all, measure.vars = c('right', 'sei', 'fin', 'humpback'), variable.name = 'species', value.name = 'score')
-
-# remove absences to reduce data frame size
-detections = detections[detections$score!='not detected',]
-
-# add number column
-detections$number = NA
-
-# config data types
-detections = config_observations(detections)
-
-# save output
-saveRDS(detections, paste0(output_dir, 'dcs_detections.rds'))
