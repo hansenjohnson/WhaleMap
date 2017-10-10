698502354b83d8f12060c0db9e6066fd1afc3875
hansen <hansenjohnson@users.noreply.github.com>
2017-10-10 12:42:27 -0300

    include processing of 2017 dfo tracks

--- proc_data.R
+++ proc_data.R
@@ -10,10 +10,10 @@ source('functions/proc_map_polygons.R')
 
 # process each platform ---------------------------------------------------
 
-# narwc (all narwc historical data)
+# all narwc (historical tracks and sightings 1951 - 2015)
 source('functions/proc_narwc.R')
 
-# dcs (gliders, buoys, etc)
+# all dcs (gliders, buoys, etc tracks and detections)
 source('functions/proc_archived_dcs.R')
 source('functions/proc_live_dcs.R')
 source('functions/proc_dcs_latest_position.R')
@@ -21,11 +21,14 @@ source('functions/proc_dcs_latest_position.R')
 # # noaa (noaa plane) 2015 (THIS IS DUPLICATED BY NARWC HISTORICAL DATA, SO NOT INCLUDING ANYMORE)
 # source('functions/proc_noaa_2015.R')
 
-# shelagh (cwi boat) 2016
+# 2016 shelagh (vessel) tracks
 source('functions/proc_shelagh_2016.R')
 
-# 2017 tracklines
+# 2017 aerial survey tracks
 source('functions/proc_noaa_tracks_2017.R')
+source('functions/proc_dfo_tracks_2017.R')
+
+# 2017 shelagh (vessel) tracks
 source('functions/proc_shelagh_tracks_2017.R')
 
 # 2017 sightings
