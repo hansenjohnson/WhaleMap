93c7c95e05c5fd22096f92d9ec0f437df1345614
Hansen Johnson <hansen.johnson@dal.ca>
2017-10-10 08:33:27 -0300

    function to determine latest glider position

---
+++ functions/proc_dcs_latest_position.R
@@ -0,0 +1,29 @@
+# extract latest position(s) from live glider data
+
+# user input --------------------------------------------------------------
+
+infile = 'data/interim/dcs_live_detections.rds'
+
+outfile = 'data/processed/dcs_live_latest_position.rds'
+
+# process -----------------------------------------------------------------
+
+# read in data
+tracks = readRDS(infile)
+
+# remove NAs
+dep = tracks[complete.cases(tracks),]
+
+# split tracks by deployment
+dep = split(dep, dep$id)
+
+# determine latest observation from each deployment
+latest = lapply(dep, function(x){
+  x[nrow(x),]
+})
+
+# flatten list
+latest = do.call(rbind,latest)
+
+# save output
+saveRDS(latest, file = outfile)
