1df7dffec6a005a884dfb878fab12cd67fe18764
Hansen Johnson <hansen.johnson@dal.ca>
2017-10-10 08:32:45 -0300

    separate functions for live and archived data processing

--- functions/proc_archived_dcs.R
+++ functions/proc_archived_dcs.R
@@ -1,18 +1,18 @@
-# process and save all live dcs (glider, buoy, etc) deployment tracklines and detections
+# process and save all archived dcs (glider, buoy, etc) deployment tracklines and detections
 
 # user input --------------------------------------------------------------
 
 # directory to look for files
-data_dir = 'data/raw/dcs/live/'
+data_dir = 'data/raw/dcs/archived/'
 
 # directory for output
 output_dir = 'data/interim/'
 
 # track file
-track_fname = 'dcs_live_tracks.rds'
+track_fname = 'dcs_archived_tracks.rds'
 
 # detection file
-det_fname = 'dcs_live_detections.rds'
+det_fname = 'dcs_archived_detections.rds'
 
 # process -----------------------------------------------------------------
 
@@ -20,4 +20,4 @@ det_fname = 'dcs_live_detections.rds'
 source('functions/proc_dcs.R')
 
 # process data
-proc_dcs(data_dir = data_dir, output_dir = output_dir, det_fname = det_fname, track_fname = track_fname, ext = '-live')
+proc_dcs(data_dir = data_dir, output_dir = output_dir, det_fname = det_fname, track_fname = track_fname)
