1715042daa22af4effd19a9a40857b4cdecebee1
Hansen Johnson <hansen.johnson@dal.ca>
2017-12-17 17:15:22 -0500

    add otn200 roseway mission

--- get_archived_dcs.sh
+++ get_archived_dcs.sh
@@ -10,6 +10,7 @@ declare -A URL
 
 # assign paths to detection data for each deployment
 URL=(		
+	[2016-06-23_buoy_nybight]=http://dcs.whoi.edu/nyb0616/dmon009_html/ptracks/manual_analysis.csv		
 	[2017-08-02_slocum_otn200]=http://dcs.whoi.edu/dal0817/dal0817_otn200_html/ptracks/manual_analysis.csv	
 	[2017-10-03_slocum_dal556]=http://dcs.whoi.edu/dal1017/dal1017_dal556_html/ptracks/manual_analysis.csv
 	[2016-09-28_buoy_nomans]=http://dcs.whoi.edu/nomans0916/dmon028_html/ptracks/manual_analysis.csv
