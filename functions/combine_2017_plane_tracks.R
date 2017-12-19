3d3ac2e8602a1da12868330a9f777eb1c4b6170f
Hansen Johnson <hansen.johnson@dal.ca>
2017-12-19 11:47:31 -0500

    combine all tracks for angelia

--- functions/combine_2017_plane_tracks.R
+++ functions/combine_2017_plane_tracks.R
@@ -1,18 +1,30 @@
 # combine all 2017 flight data for angelia
 
-# process all data
-source('functions/noaa_twin_otter.R')
-source('functions/dfo_twin_otter.R')
-source('functions/tc_dash8.R')
+# libraries
+library(plyr)
+
+# functions
+source('functions/plot_track.R')
+
+# process all data - MAKE SURE THESE ARE NOT SUBSETTED
+source('functions/proc_noaa_twin_otter.R')
+source('functions/proc_dfo_twin_otter.R')
+source('functions/proc_tc_dash8.R')
 
 # read in all data
-noaa = readRDS('data/interim/noaa_twin_otter.rds')
-dfo = readRDS('data/interim/dfo_twin_otter.rds')
-tc = readRDS('data/interim/tc_dash8.rds')
+noaa = readRDS('data/interim/noaa_twin_otter_tracks.rds')
+dfo = readRDS('data/interim/dfo_twin_otter_tracks.rds')
+tc = readRDS('data/interim/tc_dash8_tracks.rds')
 
 # combine all data
 lst = list(noaa,dfo,tc)
-tracks = Reduce(function(x, y) merge(x, y, all=TRUE), lst)
+# merge files
+tracks = join_all(lst, type = 'full')
+
+# verify
+png('figures/all_tracks.png', width = 5, height = 5, units = 'in', res = 250)
+plot_track(tracks, span = 2250)
+dev.off()
 
 # write csv
 write.csv(tracks, '2017_aerial_tracks-spd-alt.csv', row.names = F)
--- functions/plot_track.R
+++ functions/plot_track.R
@@ -1,12 +1,19 @@
-plot_track = function(gps){
+plot_track = function(gps, span = 'default', verbose = F){
   library(oce)
   library(ocedata)
   data("coastlineWorldFine")
   
+  if(span=='default'){
+    span = 3 * 111 * diff(range(gps$lat))
+    if(verbose){
+      message('Using span = ', span)
+    }
+  }
+  
   plot(coastlineWorldFine, 
        clon = mean(gps$lon), 
        clat = mean(gps$lat), 
-       span = 3 * 111 * diff(range(gps$lat))
+       span = span
   )
   
   lines(gps$lon, gps$lat, col = 'blue')
