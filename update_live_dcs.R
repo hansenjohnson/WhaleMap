6f0ea5a545289badadb069546d0864fbf12bbe13
Hansen Johnson <hansen.johnson@dal.ca>
2018-03-19 10:07:18 -0400

    update `update_live_dcs` to re-read entire deployment to be always consistent with dcs.edu

--- update_live_dcs.R
+++ update_live_dcs.R
@@ -29,22 +29,28 @@ tracks_live = readRDS('data/interim/dcs_live_tracks.rds')
 
 # merge and save obs ------------------------------------------------------
 
-# combine observations
+# remove live
+obs = obs[-(obs$id %in% unique(obs_live$id)),]
+
+# combine live and archived observations
 obs_merged = join(obs, obs_live, type = 'full')
 
 # remove duplicates (excluding id column to avoid keeping both live and archived)
-obs = obs_merged[which(!duplicated(obs_merged[,c(1:6, 8:12)])),]
+# obs = obs_merged[which(!duplicated(obs_merged[,c(1:6, 8:12)])),]
 
 # save
 saveRDS(obs, file = 'data/processed/observations.rds')
 
 # merge and save tracks ---------------------------------------------------
 
+# remove live
+tracks = tracks[-(tracks$id %in% unique(tracks_live$id)),]
+
 # combine tracks
 tracks_merged = join(tracks, tracks_live, type = 'full')
 
 # remove duplicates (excluding id column to avoid keeping both live and archived)
-tracks = tracks_merged[which(!duplicated(tracks_merged[,c(1:6, 8:9)])),]
+# tracks = tracks_merged[which(!duplicated(tracks_merged[,c(1:6, 8:9)])),]
 
 # save
 saveRDS(tracks, file = 'data/processed/tracks.rds')
