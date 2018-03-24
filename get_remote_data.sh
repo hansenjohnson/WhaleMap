47cc1afab2a537ca5027020256009a92f659c7d5
Hansen Johnson <hansen.johnson@dal.ca>
2018-03-24 18:44:43 -0400

    update get_remote_data to use 'make'

--- get_remote_data.sh
+++ get_remote_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # download survey data from remote repository
 
+## Get data ##
+
 # Move to project data directory
 # cd ~/Projects/WhaleMap/data/raw/ # local
 cd /srv/shiny-server/WhaleMap/data/raw/ # server
@@ -10,3 +12,9 @@ mkdir -p backups
 
 # Sightings
 rclone sync drive:2018-narw-opportunistic-sightings/ 2018_opportunistic_sightings/ --drive-formats csv --backup-dir backups --no-update-modtime -v
+
+## Update map ##
+
+# cd ~/Projects/WhaleMap/ # local
+cd /srv/shiny-server/WhaleMap/ # server
+make
