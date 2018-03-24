fc181c984184812612e107887300336522fd6bed
Hansen Johnson <hansen.johnson@dal.ca>
2018-03-24 16:53:02 -0400

    update to handle NAs from speed and altitude

--- functions/proc_dcs_latest_position.R
+++ functions/proc_dcs_latest_position.R
@@ -12,11 +12,11 @@ if(file.exists(infile)){
   # read in data
   tracks = readRDS(infile)
   
-  # remove NAs
-  dep = tracks[complete.cases(tracks),]
+  # # remove NAs
+  # dep = tracks[complete.cases(tracks),]
   
   # split tracks by deployment
-  dep = split(dep, dep$id)
+  dep = split(tracks, tracks$id)
   
   # determine latest observation from each deployment
   latest = lapply(dep, function(x){
@@ -28,6 +28,7 @@ if(file.exists(infile)){
   
   # save output
   saveRDS(latest, file = outfile)
+  
 } else {
   message('No live dcs data detected')
   
