46bf20badc56c3407d3a74a42ce50f10d7ea4deb
Hansen Johnson <hansen.johnson@dal.ca>
2018-04-17 14:33:45 -0300

    test simplify fxn

--- dev/rgeos_gsimplify.R
+++ dev/rgeos_gsimplify.R
@@ -5,9 +5,9 @@ library(rgeos)
 library(rgdal)
 
 # load data
-tracks = readRDS('data/interim/noaa_twin_otter_tracks.rds')
+tracks = readRDS('data/interim/2017_tc_dash8_tracks.rds')
 
-?rgeos::gSimplify()
+# ?rgeos::gSimplify()
 
 # split up data frame to a list of flights
 lst = split(tracks, f = tracks$id)
@@ -19,18 +19,34 @@ lst2 = lapply(lst, function(x) cbind(x$lat, x$lon))
 lns = lapply(lst2, FUN = Line)
 
 # make spatial lines data frame
-Lns = Lines(lns, ID = 'noaa')
+Lns = Lines(lns, ID = 'test')
 
-# Spatial
+# convert to spatial lines object
 Slns = SpatialLines(list(Lns))
 
-# simplify
-sSlns = gSimplify(Slns, tol = .05)
+# simply and plot ---------------------------------------------------------
 
-# compare
-plot(Slns)
-lines(sSlns, col = 'blue')
+par(mfrow=c(2,2))
 
-# save
-saveRDS(Slns, 'noaa_full.rds')
-saveRDS(sSlns, 'noaa_simple.rds')
+# original
+plot(Slns, main = paste0('Original (', object.size(Slns), ' bytes)'))
+
+# 1
+t1 = gSimplify(Slns, tol = .05)
+plot(t1, main = paste0('Tol: 0.05 (', object.size(t1), ' bytes)'))
+
+# 2
+t2 = gSimplify(Slns, tol = .1)
+plot(t2, main = paste0('Tol: 0.1 (', object.size(t2), ' bytes)'))
+
+# 3
+t3 = gSimplify(Slns, tol = .5)
+plot(t3, main = paste0('Tol: 0.5 (', object.size(t3), ' bytes)'))
+
+coordinates(t3)
+
+# convert back to lines ---------------------------------------------------
+
+df <- data.frame(len = sapply(1:length(Slns), function(i) gLength(Slns[i, ])))
+
+Sldf <- SpatialLinesDataFrame(Slns, data = df)
