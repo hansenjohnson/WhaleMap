54cc477fbe340a6171da63dfa85e4b49eadc66da
Hansen Johnson <hansen.johnson@dal.ca>
2018-06-03 14:50:41 +0200

    extra space

--- functions/proc_tracks.R
+++ functions/proc_tracks.R
@@ -9,15 +9,15 @@ tracks_list = list.files('data/interim', pattern = 'tracks', full.names = T)
 
 # read in files
 for(i in seq_along(tracks_list)){
-  
+
   # get data
   itrack = readRDS(tracks_list[[i]])
   # itrack = config_tracks(itrack)
-  
+
   if(i==1){
     tracks = itrack
   }
-  
+
   # add to list
   tracks = rbind(tracks, itrack)
 }
