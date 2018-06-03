d2eef81f756fc86f34d3f52f59a269913bb49349
Hansen Johnson <hansen.johnson@dal.ca>
2018-06-03 14:51:01 +0200

    change track plotting limits

--- functions/plot_track.R
+++ functions/plot_track.R
@@ -3,18 +3,22 @@ plot_track = function(gps, span = 'default', verbose = F){
   library(ocedata)
   data("coastlineWorldFine")
   
+  # determine limits
   if(span=='default'){
-    span = 3 * 111 * diff(range(gps$lat, na.rm = T))
+    # span = 3 * 111 * diff(range(gps$lat, na.rm = T))
+    span = 6 * 111 * diff(range(gps$lat, na.rm = T))
     if(verbose){
       message('Using span = ', span)
     }
   }
   
+  # make map
   plot(coastlineWorldFine, 
        clon = mean(gps$lon, na.rm = T), 
        clat = mean(gps$lat, na.rm = T), 
        span = span
   )
   
+  # add lines
   lines(gps$lon, gps$lat, col = 'blue')
 }
