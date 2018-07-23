c1b16bf0fe644cb2a7a3044072c2d17685e07ca0
Hansen Johnson <hansen.johnson@dal.ca>
2018-07-23 14:45:33 -0300

    read additional files

--- functions/proc_2018_cwi_jdmartin.R
+++ functions/proc_2018_cwi_jdmartin.R
@@ -18,7 +18,7 @@ source('functions/subsample_gps.R')
 spp_key = read.csv(paste0(data_dir, '/species_key.csv'))
 
 # list data files
-flist = list.files(data_dir, pattern = '*CWI-V.csv$', full.names = T)
+flist = list.files(data_dir, pattern = '*CWI-V.*.csv$', full.names = T)
 
 TRK = list()
 SIG = list()
