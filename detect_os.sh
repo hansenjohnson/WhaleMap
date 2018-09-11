082efa71e326b3e3ffd17d22870acb2a99b1ecc2
Hansen Johnson <hansen.johnson@dal.ca>
2018-09-11 17:22:22 -0300

    fix syntax error in if statement

--- detect_os.sh
+++ detect_os.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Determine location
 unamestr=`uname`
-if [[ "$unamestr"=='Darwin' ]]; then
-	echo /Users/hansenjohnson/Projects/WhaleMap # local
-	echo /Users/hansenjohnson
-elif [[ "$unamestr"=='Linux' ]]; then
+if [[ "$unamestr" == 'Linux' ]]; then
 	echo /srv/shiny-server/WhaleMap # server
 	echo /home/hansen
+elif [[ "$unamestr" == 'Darwin' ]]; then
+	echo /Users/hansenjohnson/Projects/WhaleMap # local
+	echo /Users/hansenjohnson
 fi
