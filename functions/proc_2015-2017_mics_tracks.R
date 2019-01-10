5d9cdd11b83feb510780f935cd7dd12e2c99c048
Hansen Johnson <hansen.johnson@dal.ca>
2019-01-10 08:31:29 -0500

    process 2015 mics data from raw gpx

--- functions/proc_2015-2017_mics_tracks.R
+++ functions/proc_2015-2017_mics_tracks.R
@@ -29,6 +29,79 @@ source('functions/on_server.R')
 # plot tracks?
 plot_tracks = !on_server()
 
+# 2015 --------------------------------------------------------------------
+
+# list files to process
+flist = list.files(data_dir, pattern = '.gpx$', full.names = T, recursive = T, ignore.case = T)
+
+# list to hold loop output
+TRK = list()
+
+# read and format data ----------------------------------------------------
+
+# read files
+for(i in seq_along(flist)){
+  
+  if(file.size(flist[i])<51200){
+    next
+  }
+  
+  # read in file
+  tmp = readOGR(dsn = flist[i], layer="track_points", verbose = F)
+  
+  # convert to data frame
+  tmp = as.data.frame(tmp)
+  
+  # dummy variable for speed
+  tmp$speed = NA
+  
+  # select and rename important columns
+  tmp = data.frame(tmp$time, tmp$coords.x1, tmp$coords.x2, tmp$speed, tmp$ele)
+  colnames(tmp) = c('time', 'lon', 'lat', 'speed', 'altitude')
+  
+  # remove columns without timestamp
+  tmp = tmp[which(!is.na(tmp$time)),]
+  
+  # add timestamp
+  tmp$time = as.POSIXct(tmp$time, format = '%Y/%m/%d %H:%M:%OS', tz = 'UTC')
+  
+  # subsample (use default subsample rate)
+  tracks = subsample_gps(gps = tmp)
+  
+  # isolate vessel name
+  vname = tolower(strsplit(file_path_sans_ext(basename(flist[i])), '-')[[1]][2])
+  
+  # add metadata
+  tracks$date = as.Date(tracks$time)
+  tracks$yday = yday(tracks$date)
+  tracks$year = year(tracks$date)
+  tracks$platform = 'vessel'
+  tracks$name = paste0('mics_', vname)
+  tracks$id = paste(tracks$date, tracks$platform, tracks$name, sep = '_')
+  
+  # plot track
+  if(plot_tracks){
+    plot_save_track(tracks, flist[i])
+  }
+  
+  # add to list
+  TRK[[i]] = tracks
+  
+  # catch null error
+  if(is.null(TRK[[i]])){stop('Track in ', flist[i], ' not processed correctly!')}
+  
+}
+
+# combine and save --------------------------------------------------------
+
+# combine all flights
+TRACKS = do.call(rbind, TRK)
+
+# config flight data
+t1 = config_tracks(TRACKS)
+
+# 2016-2017 ---------------------------------------------------------------
+
 # list files to process
 flist = list.files(data_dir, pattern = '_final.xlsx$', full.names = T, recursive = T, ignore.case = T)
 
@@ -89,7 +162,7 @@ for(i in seq_along(flist)){
   
 }
 
-# combine and save --------------------------------------------------------
+# combine --------------------------------------------------------
 
 # combine all flights
 TRACKS = do.call(rbind, TRK)
@@ -98,7 +171,12 @@ TRACKS = do.call(rbind, TRK)
 tracks = config_tracks(TRACKS)
 
 # remove inappropriate points from 2017 (likely waypoint layer)
-tracks = tracks[!tracks$id %in% c('2017-07-27_vessel_mics_mistral', '2017-07-05_vessel_mics_rafale'),]
+t2 = tracks[!tracks$id %in% c('2017-07-27_vessel_mics_mistral', '2017-07-05_vessel_mics_rafale'),]
+
+# merge and save --------------------------------------------------------------------
+
+# merge t1 and t2
+tracks = rbind(t1, t2)
 
 # save
 saveRDS(tracks, paste0(output_dir, ofile))
