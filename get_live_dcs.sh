8200b7b22dd8b1aa5000de1cbd757e220ce77389
Hansen Johnson <hansen.johnson@dal.ca>
2019-01-24 09:15:37 -0500

    add hatteras gliders

--- get_live_dcs.sh
+++ get_live_dcs.sh
@@ -19,8 +19,10 @@ declare -A URL
 # assign paths to detection data for each deployment
 URL=(
 	# live
-	[2018-12-01_slocum_we03]=http://dcs.whoi.edu/gom1218/gom1218_we03_html/ptracks/manual_analysis.csv		
+	[2018-12-01_slocum_we03]=http://dcs.whoi.edu/gom1218/gom1218_we03_html/ptracks/manual_analysis.csv
 	[2018-02-13_buoy_nybight]=http://dcs.whoi.edu/nyb0218/nyb0218_buoy_html/ptracks/manual_analysis.csv
+	[2019-01-22_slocum_we14]=http://dcs.whoi.edu/hatteras0119/hatteras0119_we14_html/ptracks/manual_analysis.csv
+	[2019-01-23_slocum_we15]=http://dcs.whoi.edu/hatteras0119/hatteras0119_we15_html/ptracks/manual_analysis.csv
 )
 
 # download data
