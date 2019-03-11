7c73b45cbb15e1b5e7d96e3b5d904d9c014c9778
Hansen Johnson <hansen.johnson@dal.ca>
2019-03-11 10:20:34 -0300

    no longer track `wrk`

--- wrk/build_static_map.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# build static map using latest data for display on main webpage
-
-# move to map directory
-cd /srv/shiny-server/WhaleMap
-
-# run build script
-Rscript build_static_map.R
