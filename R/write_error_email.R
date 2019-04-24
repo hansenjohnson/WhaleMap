f14a638b617c7405049f3341c04c9c28d3e8928a
Hansen Johnson <hansen.johnson@dal.ca>
2019-04-24 08:44:58 -0300

    do not use fxn name for var

--- R/write_error_email.R
+++ R/write_error_email.R
@@ -14,8 +14,8 @@ email_file = 'error_email.txt'
 # process -----------------------------------------------------------------
 
 # read in status file
-st = read.csv(status_file, header = FALSE, 
-              col.names = c('script', 'status'), 
+st = read.csv(status_file, header = FALSE,
+              col.names = c('script', 'status'),
               stringsAsFactors = FALSE)
 
 # find error(s)
@@ -23,46 +23,46 @@ er = grep(pattern = 'ERROR', x = st$status)
 
 # create and send email if error exists
 if(length(er)!=0){
-  
+
   # only create and send email if it has not been sent already
   if(!file.exists(email_file)){
-    
+
     # read in lookup table
     id = read.csv(index_file, header = TRUE, stringsAsFactors = FALSE)
-    
+
     # find bad script(s) in lookup table
     bad = match(st$script[er], id$script)
-    
+
     # remove those missing from looking table
     bad = bad[!is.na(bad)]
-    
+
     # only report one error at a time
     bad = bad[1]
-    
+
     # report error
     message('Error found in: ', id$script[bad])
-    
+
     # define email list file
     efile = id$email_list[bad]
-    
+
     # read email list for bad script
     if(file.exists(efile)){
       message('Sending to recipients listed in: ', efile)
-      emails = read.csv(efile, header = TRUE, stringsAsFactors = FALSE)$email  
+      emails = read.csv(efile, header = TRUE, stringsAsFactors = FALSE)$email
     } else {
       message('Could not find ', efile, ', so deferring to the default email list here: ', default_email_list)
-      emails = read.csv(default_email_list, header = TRUE, stringsAsFactors = FALSE)$email  
+      emails = read.csv(default_email_list, header = TRUE, stringsAsFactors = FALSE)$email
     }
-    
+
     # email pieces
     subject = paste0("Subject: WhaleMap Error (", format(Sys.time(), '%b-%d %H:%M %Z'),"): Cannot process data from ", id$name[bad])
     from = "From: hansen.johnson@dal.ca"
     to = paste0("Bcc: ", paste(emails, collapse = ';'))
-    message = paste0(
-      "
-WhaleMap could not process ", id$name[bad], " correctly! 
+    txt = paste0(
+"
+WhaleMap could not process ", id$name[bad], " correctly!
 
-Please check your data for errors. Common issues include typos in the file name, mixing time or lat/lon conventions, or using unknown species codes. It can be very helpful to check the formatting of a previous record. 
+Please check your data for errors. Common issues include typos in the file name, mixing time or lat/lon conventions, or using unknown species codes. It can be very helpful to check the formatting of a previous record.
 
 If you have tried to make a correction, head to https://whalemap.ocean.dal.ca/WhaleMap to see if it was successful. WhaleMap updates every 15 minutes, so it should not take long to see your changes. You can also scroll down on WhaleMap to check the status table. If you see an error message next to your platform name, there is still a problem.
 
@@ -73,21 +73,21 @@ If you cannot find an error and/or suspect the problem is related to WhaleMap, p
     
     # write email file
     fileConn<-file(email_file)
-    writeLines(c(to, subject, from, message), fileConn)
+    writeLines(c(to, subject, from, txt), fileConn)
     close(fileConn)
-    
+
     # send email
     system(paste0('sendmail -vt < ', email_file))
-    
+
     message('Error message sent on: ', Sys.time())
-    
+
   } else {
     message('Error message already sent')
   }
-  
+
 } else {
   # no error found
-  
+
   # remove email file if it exists
   if(file.exists(email_file)){
     file.remove(email_file)
@@ -96,8 +96,3 @@ If you cannot find an error and/or suspect the problem is related to WhaleMap, p
     message('No errors to report')
   }
 }
-
-
-
-
-
