d95b51bbad03dd8929106390d3f082270c10ac12
Hansen Johnson <hansen.johnson@dal.ca>
2020-05-22 13:39:05 -0300

    add new stellwagen glider

--- src/get_live_dcs.sh
+++ src/get_live_dcs.sh
@@ -18,6 +18,7 @@ declare -A URL
 
 # assign paths to detection data for each deployment
 URL=(		
+	[2020-05-21_slocum_we04]=http://dcs.whoi.edu/sbnms0520/sbnms0520_we04_html/ptracks/manual_analysis.csv
 	[2020-01-15_buoy_nybnw]=http://dcs.whoi.edu/nybnw0120/nybnw0120_buoy_html/ptracks/manual_analysis.csv
 	[2020-01-15_buoy_nybse]=http://dcs.whoi.edu/nybse0120/nybse0120_buoy_html/ptracks/manual_analysis.csv
 )
