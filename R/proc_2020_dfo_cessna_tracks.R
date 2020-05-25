b0f90e456cfb4f661763511b9f8293fa7dc40916
Hansen Johnson <hansen.johnson@dal.ca>
2020-05-25 09:41:15 -0300

    update dfo cessna processing to use more efficient gpx reading function

--- R/proc_2020_dfo_cessna_tracks.R
+++ R/proc_2020_dfo_cessna_tracks.R
@@ -14,16 +14,8 @@ output_dir = 'data/interim/'
 
 # setup -------------------------------------------------------------------
 
-# libraries
-suppressPackageStartupMessages(library(lubridate))
-suppressPackageStartupMessages(library(rgdal))
-suppressPackageStartupMessages(library(tools))
-
 # functions
 source('R/functions.R')
-
-# plot tracks?
-plot_tracks = !on_server()
 
 # list files to process
 flist = list.files(data_dir, pattern = '.gpx', full.names = T, recursive = T, ignore.case = T)
@@ -38,28 +30,17 @@ if(length(flist!=0)){
   # read files
   for(i in seq_along(flist)){
     
-    if(file.size(flist[i])<51200){
-      next
-    }
-    
     # read in file
-    tmp = readOGR(dsn = flist[i], layer="track_points", verbose = F)
-    
-    # convert to data frame
-    tmp = as.data.frame(tmp)
+    tmp = read_GPX(flist[i])
     
     # dummy variable for speed
     tmp$speed = NA
-    
-    # select and rename important columns
-    tmp = data.frame(tmp$time, tmp$coords.x1, tmp$coords.x2, tmp$speed, tmp$ele)
-    colnames(tmp) = c('time', 'lon', 'lat', 'speed', 'altitude')
     
     # remove columns without timestamp
     tmp = tmp[which(!is.na(tmp$time)),]
     
     # add timestamp
-    tmp$time = as.POSIXct(tmp$time, format = '%Y/%m/%d %H:%M:%OS', tz = 'UTC')
+    tmp$time = with_tz(as.POSIXct(tmp$time, tz = 'America/Halifax'), tzone = 'UTC')
     
     # subsample (use default subsample rate)
     tracks = subsample_gps(gps = tmp)
@@ -71,11 +52,6 @@ if(length(flist!=0)){
     tracks$platform = 'plane'
     tracks$name = 'dfo_cessna'
     tracks$id = paste(tracks$date, tracks$platform, tracks$name, sep = '_')
-    
-    # plot track
-    if(plot_tracks){
-      plot_save_track(tracks, flist[i])
-    }
     
     # add to list
     TRK[[i]] = tracks
