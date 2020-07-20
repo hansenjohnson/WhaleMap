4fdb8e0af1bd62880f0211a1d5a976e2d044d52d
Hansen Johnson <hansen.johnson@dal.ca>
2020-07-20 08:06:06 -0300

    add cabot and remove we04 deployment

--- src/get_live_dcs.sh
+++ src/get_live_dcs.sh
@@ -18,8 +18,8 @@ declare -A URL
 
 # assign paths to detection data for each deployment
 URL=(		
+	[2020-07-17_slocum_cabot]=http://dcs.whoi.edu/dal0720/dal0720_cabot_html/ptracks/manual_analysis.csv
 	[2020-06-02_slocum_we16]=http://dcs.whoi.edu/neocs0620/neocs0620_we16_html/ptracks/manual_analysis.csv
-	[2020-05-21_slocum_we04]=http://dcs.whoi.edu/sbnms0520/sbnms0520_we04_html/ptracks/manual_analysis.csv
 	[2020-01-15_buoy_nybnw]=http://dcs.whoi.edu/nybnw0120/nybnw0120_buoy_html/ptracks/manual_analysis.csv
 	[2020-01-15_buoy_nybse]=http://dcs.whoi.edu/nybse0120/nybse0120_buoy_html/ptracks/manual_analysis.csv
 )
