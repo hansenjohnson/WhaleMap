c4ff7a44ffd24bea2f34f85b4c0a271c0c45a274
Hansen Johnson <hansen.johnson@dal.ca>
2021-04-19 08:18:27 -0300

    query NOAA API for BOEM layers

--- R/proc_boem.R
+++ R/proc_boem.R
@@ -3,53 +3,66 @@
 
 # input -------------------------------------------------------------------
 
-# input directory
-gis_dir = 'data/raw/boem/'
-
 # output directory
-output_dir = 'data/processed/'
+ofile = 'data/processed/boem.rda'
 
 # setup -------------------------------------------------------------------
 
 # libraries
-suppressPackageStartupMessages(library(rgdal))
 suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(rgeos))
+suppressPackageStartupMessages(library(sf))
 
-# common projection
-ref = "+proj=longlat +init=epsg:3857"
+## Query steps ##
+# 1. Go to specific layer on ArcGIS rest api: https://services1.arcgis.com/Hp6G80Pky0om7QvQ/ArcGIS/rest/services/BOEM_Wind_Planning_and_Lease_Areas/FeatureServer
+# 2. Click 'query'
+# 3. Add the following fields:
+# Where: 1=1
+# Out Fields: *
+# Format: GEOJSON
+# 4. Click 'query' and paste url below
+
+# wind lease areas 
+query1 = 'https://services1.arcgis.com/Hp6G80Pky0om7QvQ/ArcGIS/rest/services/BOEM_Wind_Planning_and_Lease_Areas/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token='
+
+# wind MHK leases and planning areas 
+query2 = 'https://services1.arcgis.com/Hp6G80Pky0om7QvQ/ArcGIS/rest/services/BOEM_Wind_Planning_and_Lease_Areas/FeatureServer/1/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token='
+
+# wind planning areas
+query3 = 'https://services1.arcgis.com/Hp6G80Pky0om7QvQ/ArcGIS/rest/services/BOEM_Wind_Planning_and_Lease_Areas/FeatureServer/2/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token='
 
 # process -----------------------------------------------------------------
 
-# read ny zone
-ny = readOGR(paste0(gis_dir, '/NY WEAs_new as of 3_29_21/')) %>%
-  spTransform(ref)
-
-# add metadata
-ny@data$ID = 'NY Wind Energy Area'
-ny@data$Id = NULL
-
-# read project areas
-pj = readOGR(paste0(gis_dir, '/Project Areas/')) %>%
-  spTransform(ref)
-
-# add metadata
-pj@data$ID = paste0('Project area: ', pj@data$NAME)
-pj@data$NAME = NULL
-pj@data$Acres = NULL
+# read
+q1 = st_read(query1) %>%
+  dplyr::select(INFO, geometry)
+q2 = st_read(query2) %>%
+  dplyr::select(INFO, geometry)
+q3 = st_read(query3) %>%
+  dplyr::select(INFO, geometry)
 
 # combine
-boem = rbind(ny, pj)
+df = rbind(q1,q2,q3)
 
-# test with leaflet
+# crop to east coast
+df = st_crop(x = df, y = c(xmin = -82, xmax = -67, ymin = 26, ymax = 60))
+
+# calculate common border
+zones = unique(df$INFO)
+BOEM = vector('list', length = length(zones))
+for(ii in seq_along(zones)){
+  BOEM[[ii]] = st_as_sf(st_union(st_buffer(df[df$INFO == zones[ii],], dist = 0.0001))) %>%
+    mutate(ID = zones[ii])
+}
+
+# flatten list
+boem = bind_rows(BOEM)
+
+# # plot to test
 # library(leaflet)
 # leaflet() %>%
 #   addTiles() %>%
-#   addLayersControl(
-#     overlayGroups = c('boem')
-#     ) %>%
-#   addPolygons(data = boem, color = 'blue', popup = ~paste0(ID),
-#                group = 'boem',weight = 2)
+#   addPolygons(data = df, color = 'red', popup = ~paste0(INFO), weight = 2) %>%
+#   addPolygons(data = boem, color = 'blue', popup = ~paste0(ID), weight = 2)
 
 # save
-save(boem, file = paste0(output_dir, 'boem.rda'))
+save(boem, file = ofile)
--- src/get_boem_data.sh
+++ src/get_boem_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# download map data from BOEM
+# process map data from BOEM
 
 # Extract OS name
 unamestr=`uname`
@@ -12,15 +12,6 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
 	DESTDIR=/Users/hansenjohnson/Projects/WhaleMap # local
 	SSHDIR=/Users/hansenjohnson
 fi
-
-# Determine raw data directory
-DATADIR=${DESTDIR}/data/raw/
-
-# Move to data directory
-cd ${DATADIR}
-
-# Sync BOEM development areas Google drive
-rclone sync drive:"BOEM shapefiles" "boem/" --drive-shared-with-me --drive-formats csv --backup-dir backups -v --exclude *.JPG --exclude *.jpg --exclude *.mp4 --exclude *.pdf
 
 # move to project directory
 cd ${DESTDIR}
