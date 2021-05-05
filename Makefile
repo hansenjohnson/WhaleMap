d977c0d9ce94426525bb37879c5532d24b1041ba
Hansen Johnson <hansen.johnson@dal.ca>
2021-05-05 08:03:37 -0300

    add zwf cessna

--- Makefile
+++ Makefile
@@ -52,6 +52,7 @@ data/processed/tracks.rds : R/proc_tracks.R \
 														data/interim/2021_dfo_cessna_yob_tracks.rds \
 														data/interim/2021_tc_dash8_tracks.rds \
 														data/interim/2021_dfo_twin_otter_tracks.rds \
+														data/interim/2021_dfo_cessna_zwf_tracks.rds \
 														data/interim/dcs_archived_tracks.rds \
 														data/interim/dcs_live_tracks.rds
 	$(report_error)
@@ -304,6 +305,12 @@ data/interim/2021_dfo_twin_otter_tracks.rds : R/proc_2021_dfo_twin_otter_tracks.
 	Rscript $<
 	$(remove_error)
 
+# 2021 DFO Cessna ZWF tracks
+data/interim/2021_dfo_cessna_zwf_tracks.rds : R/proc_2021_dfo_cessna_zwf_tracks.R data/raw/2021_whalemapdata/DFO_Cessna_ZWF/*
+	$(report_error)
+	Rscript $<
+	$(remove_error)
+
 # DCS archived tracks
 data/interim/dcs_archived_tracks.rds : R/proc_archived_dcs.R data/raw/dcs/archived/*/*
 	$(report_error)
@@ -364,6 +371,7 @@ data/processed/observations.rds : R/proc_observations.R \
 																	data/interim/2021_tc_dash8_sightings.rds \
 																	data/interim/2021_dfo_twin_otter_sightings.rds \
 																	data/interim/2021_dfo_opportunistic_sightings.rds \
+																	data/interim/2021_dfo_cessna_zwf_sightings.rds \
 																	data/interim/sas_sightings.rds \
 																	data/interim/dcs_archived_detections.rds \
 																	data/interim/dcs_live_detections.rds
@@ -620,6 +628,12 @@ data/interim/2021_tc_dash8_sightings.rds : R/proc_2021_tc_dash8_sightings.R data
 
 # 2021 DFO twin otter sightings
 data/interim/2021_dfo_twin_otter_sightings.rds : R/proc_2021_dfo_twin_otter_sightings.R data/raw/2021_whalemapdata/DFO_twin_otter/*/*
+	$(report_error)
+	Rscript $<
+	$(remove_error)
+
+# 2021 DFO cessna ZWF sightings
+data/interim/2021_dfo_cessna_zwf_sightings.rds : R/proc_2021_dfo_cessna_zwf_sightings.R data/raw/2021_whalemapdata/DFO_Cessna_ZWF/*
 	$(report_error)
 	Rscript $<
 	$(remove_error)
---
+++ R/proc_2021_dfo_cessna_zwf_tracks.R
@@ -0,0 +1,79 @@
+## proc_2021_dfo_cessna_zwf_tracks ##
+# Process gps data from DFO cessna survey plane
+
+# user input --------------------------------------------------------------
+
+# data directory
+data_dir = 'data/raw/2021_whalemapdata/DFO_Cessna_ZWF/'
+
+# output file name
+ofile = '2021_dfo_cessna_zwf_tracks.rds'
+
+# output directory
+output_dir = 'data/interim/'
+
+# setup -------------------------------------------------------------------
+
+# functions
+source('R/functions.R')
+
+# list files to process
+flist = list.files(data_dir, pattern = '.gpx', full.names = T, recursive = T, ignore.case = T)
+
+# list to hold loop output
+TRK = vector('list', length = length(flist))
+
+# read and format data ----------------------------------------------------
+
+if(length(flist!=0)){
+  
+  # read files
+  for(i in seq_along(flist)){
+    
+    # read in file
+    tmp = read_GPX(flist[i])
+    
+    # dummy variable for speed
+    tmp$speed = NA
+    
+    # remove columns without timestamp
+    tmp = tmp[which(!is.na(tmp$time)),]
+    
+    # add timestamp
+    tmp$time = with_tz(as.POSIXct(tmp$time, tz = 'America/Halifax'), tzone = 'UTC')
+    
+    # subsample (use default subsample rate)
+    tracks = subsample_gps(gps = tmp)
+    
+    # add metadata
+    tracks$date = as.Date(tracks$time)
+    tracks$yday = yday(tracks$date)
+    tracks$year = year(tracks$date)
+    tracks$platform = 'plane'
+    tracks$name = 'dfo_cessna_zwf'
+    tracks$id = paste(tracks$date, tracks$platform, tracks$name, sep = '_')
+    
+    # add to list
+    TRK[[i]] = tracks
+    
+    # catch null error
+    if(is.null(TRK[[i]])){stop('Track in ', flist[i], ' not processed correctly!')}
+    
+  }
+  
+  # combine all flights
+  TRACKS = bind_rows(TRK)
+  
+} else {
+  
+  # assign empty data frame
+  TRACKS = data.frame()
+}
+
+# combine and save --------------------------------------------------------
+
+# config flight data
+tracks = config_tracks(TRACKS)
+
+# save
+saveRDS(tracks, paste0(output_dir, ofile))
