4d469f6facff8929dffef71e4da2e2d1980df9bc
Hansen Johnson <hansen.johnson@dal.ca>
2021-06-02 07:29:58 -0300

    add nyb buoys and archive others

--- src/get_archived_dcs.sh
+++ src/get_archived_dcs.sh
@@ -18,6 +18,9 @@ declare -A URL
 
 # assign paths to detection data for each deployment
 URL=(
+  [2020-12-20_slocum_we03]=http://dcs.whoi.edu/gom1220/gom1220_we03_html/ptracks/manual_analysis.csv
+	[2020-12-03_buoy_ncch]=http://dcs.whoi.edu/ncch1220/ncch1220_ncch_html/ptracks/manual_analysis.csv
+	[2020-01-15_buoy_nybnw]=http://dcs.whoi.edu/nybnw0120/nybnw0120_buoy_html/ptracks/manual_analysis.csv
 	[2020-07-31_buoy_mamv]=http://dcs.whoi.edu/mamv0720/mamv0720_mamv_html/ptracks/manual_analysis.csv
 	[2020-07-30_buoy_njatl]=http://dcs.whoi.edu/njatl0720/njatl0720_njatl_html/ptracks/manual_analysis.csv
   	[2021-02-08_slocum_ru34]=http://dcs.whoi.edu/rutgers0221/rutgers0221_ru34_html/ptracks/manual_analysis.csv
