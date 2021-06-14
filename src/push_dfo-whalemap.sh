4936c3376ab8f62688e979675dbb72bb88400c09
Hansen Johnson <hansen.johnson@dal.ca>
2021-06-14 19:10:25 -0300

    add server-only dfo data transmission

--- src/push_dfo-whalemap.sh
+++ src/push_dfo-whalemap.sh
@@ -6,18 +6,24 @@ unamestr=`uname`
 
 # Define OS-specific paths
 if [[ "$unamestr" == 'Linux' ]]; then
-	DESTDIR=/srv/shiny-server/WhaleMap # server
-	SSHDIR=/home/hansen
+	
+	# Move to project directory
+	cd /srv/shiny-server/WhaleMap
+
+	# Write dfo data
+	Rscript R/write_dfo-whalemap_data.R
+
+	# copy output data back to Google Drive
+	rclone copy "shared/dfo-whalemap/" whalemapdata:"WhaleMapData_Exchange/"
+
 elif [[ "$unamestr" == 'Darwin' ]]; then
-	DESTDIR=/Users/hansenjohnson/Projects/WhaleMap # local
-	SSHDIR=/Users/hansenjohnson
+
+	# Move to project directory
+	cd /Users/hansenjohnson/Projects/WhaleMap
+
+	# touch
+	touch shared/dfo-whalemap/obs_for_dfo.csv
+	touch shared/dfo-whalemap/trk_for_dfo.csv
+
+	echo "Note: will only process and send data to DFO-WhaleMap from server"	
 fi
-
-# Move to project directory
-cd ${DESTDIR}
-
-# Write dfo data
-Rscript R/write_dfo-whalemap_data.R
-
-# copy output data back to Google Drive
-rclone copy "shared/dfo-whalemap/" whalemapdata:"WhaleMapData_Exchange/"
