96777c6daacaab5714dbfed189c683c57d57300e
Hansen Johnson <hansen.johnson@dal.ca>
2021-07-27 08:52:44 -0300

    read dates from filename

--- R/proc_2021_tc_dash8_tracks.R
+++ R/proc_2021_tc_dash8_tracks.R
@@ -22,9 +22,6 @@ suppressMessages(library(tools))
 # functions
 source('R/functions.R')
 
-# plot tracks?
-plot_tracks = !on_server()
-
 # list files to process
 flist = list.files(data_dir, pattern = '^(\\d{8})_Dash8_tracklog.*.csv$', full.names = T, recursive = T)
 
@@ -39,16 +36,25 @@ for(i in seq_along(flist)){
   # read in file
   tmp = read.csv(flist[i], skip = 2)
   
+  # determine flight date from file name
+  fdate = as.Date(x = dirname(flist[i]), format = paste0(data_dir, '/%Y%m%d'))
+  ftime = as.POSIXct(paste0(fdate, ' 12:00:00 UTC'), tz = 'UTC')
+  
   # select and rename important columns
-  tmp = data.frame(tmp$Timestamp, tmp$Longitude, tmp$Latitude, tmp$Speed, tmp$Altitude)
-  colnames(tmp) = c('time', 'lon', 'lat', 'speed', 'altitude')
-  
-  # Correct for variable precision (or errors) in gps
-  f = roundTen(tmp$time)/10^9
-  tmp$time = tmp$time/f
-  
-  # add timestamp
-  tmp$time = as.POSIXct(tmp$time, origin = '1970-01-01', tz = 'UTC', usetz=TRUE)
+  tmp = tmp %>%
+    transmute(
+      date = fdate,
+      time = ftime,
+      lat = Latitude,
+      lon = Longitude,
+      speed = Speed,
+      altitude = Altitude,
+      yday = yday(date),
+      year = year(date),
+      platform = 'plane',
+      name = 'tc_dash8',
+      id = paste(date, platform, name, sep = '_')
+    )
   
   # remove columns without timestamp
   tmp = tmp[!is.na(tmp$time),]
@@ -59,19 +65,6 @@ for(i in seq_along(flist)){
   
   # subsample (use default subsample rate)
   tracks = subsample_gps(gps = tmp)
-  
-  # add metadata
-  tracks$date = as.Date(tracks$time)
-  tracks$yday = yday(tracks$date)
-  tracks$year = year(tracks$date)
-  tracks$platform = 'plane'
-  tracks$name = 'tc_dash8'
-  tracks$id = paste(tracks$date, tracks$platform, tracks$name, sep = '_')
-  
-  # plot track
-  if(plot_tracks){
-    plot_save_track(tracks, flist[i])
-  }
   
   # add to list
   TRK[[i]] = tracks
