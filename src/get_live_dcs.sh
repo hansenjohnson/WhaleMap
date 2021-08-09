c938b540ac464d5cf591a204af9f6e8235cbdc1a
Hansen Johnson <hansen.johnson@dal.ca>
2021-08-09 08:53:30 -0300

    fix url typo

--- src/get_live_dcs.sh
+++ src/get_live_dcs.sh
@@ -19,7 +19,7 @@ declare -A URL
 # assign paths to detection data for each deployment
 URL=(
 	[2021-07-27_buoy_njatl]=http://dcs.whoi.edu/njatl0721/njatl0721_njatl_html/ptracks/manual_analysis.csv
-	[2021-07-29_buoy_mamv]=http://dcs.whoi.edu/mamv0721/mamv0721_mamv_html/ptracks/ptracks/manual_analysis.csv
+	[2021-07-29_buoy_mamv]=http://dcs.whoi.edu/mamv0721/mamv0721_mamv_html/ptracks/manual_analysis.csv
 	[2021-07-19_slocum_scotia]=http://dcs.whoi.edu/dal0721/dal0721_scotia_html/ptracks/manual_analysis.csv
   	[2021-07-06_slocum_cabot]=http://dcs.whoi.edu/dal0721/dal0721_cabot_html/ptracks/manual_analysis.csv
   	[2021-05-27_buoy_mdoc]=http://dcs.whoi.edu/mdoc0521/mdoc0521_mdoc_html/ptracks/manual_analysis.csv
