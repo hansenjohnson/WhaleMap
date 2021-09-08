fd94b75896c23857d54b194d49c069b8226861f3
Hansen Johnson <hansen.johnson@dal.ca>
2021-09-08 08:16:15 -0300

    add UTC to timestamps

--- server.R
+++ server.R
@@ -915,7 +915,7 @@ function(input, output, session){
                                       paste0("Platform: ", platform),
                                       paste0("Name: ", name),
                                       paste0("Date: ", as.character(date)),
-                                      paste0("Time: ", as.character(format(time, '%H:%M:%S'))),
+                                      paste0("Time: ", as.character(format(time, '%H:%M:%S UTC'))),
                                       paste0("Position: ", as.character(lat), ', ', as.character(lon)),
                                       paste0("Source: ", source)),
                        options = markerOptions(removeOutsideVisibleBounds=T))
@@ -947,7 +947,7 @@ function(input, output, session){
                                       paste0("Platform: ", platform),
                                       paste0("Name: ", name),
                                       paste0("Date: ", as.character(date)),
-                                      paste0("Time: ", as.character(format(time, '%H:%M:%S'))),
+                                      paste0("Time: ", as.character(format(time, '%H:%M:%S UTC'))),
                                       paste0("Position: ", as.character(lat), ', ', as.character(lon)),
                                       paste0("Source: ", source)),
                        options = markerOptions(removeOutsideVisibleBounds=T))
