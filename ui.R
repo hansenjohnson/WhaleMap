55e95927b724fc88e6515f6dcd7efc0b67f6c7c8
Hansen Johnson <hjohnson@neaq.org>
2024-05-23 14:09:37 -0400

    remove CAN management grid

--- ui.R
+++ ui.R
@@ -211,9 +211,9 @@ body <- dashboardBody(
                            checkboxInput("critical_habitat_zone", 
                                          label = 'Critical habitat areas', value = T),
                            
-                           checkboxInput("full_grid", 
-                                         label = 'Management Grid', value = F),
-            
+                           # checkboxInput("full_grid", 
+                           #               label = 'Management Grid', value = F),
+                           # 
                            checkboxInput("dfo_zone", value = F,
                                          label = span("Areas subject to dynamic closure protocol for fisheries", 
                                                       tags$a(icon("info-sign", lib = "glyphicon"), 
