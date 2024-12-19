49e7527df4fd4d8fcf013154cbdccbc85718b659
Hansen Johnson <hjohnson@neaq.org>
2024-12-19 14:35:17 -0500

    convert to using csv data for hdr

--- R/proc_hdr.R
+++ R/proc_hdr.R
@@ -1,7 +1,5 @@
 ## proc_hdr ##
 # process survey data from hdr consultant group
-# requires external p7zip package
-# easiest to install on mac/homebrew with `brew install p7zip`
 
 # input -------------------------------------------------------------------
 
@@ -19,31 +17,23 @@ source('R/functions.R')
 # process -----------------------------------------------------------------
 
 # list database files
-flist = list.files(path = ddir, pattern = '*.mpk', full.names = T, recursive = T)
+olist = list.files(path = ddir, pattern = '*_sighting.csv$', full.names = T, recursive = T)
+elist = list.files(path = ddir, pattern = '*_track_points.csv$', full.names = T, recursive = T)
 
-OBS = EFF = vector('list', length(flist))
+OBS = vector('list', length(olist))
+EFF = vector('list', length(elist))
 for(ii in seq_along(OBS)){
   
   # define ifile
-  ifile = flist[[ii]]
-  
-  # define temporary directory to extract files to
-  tmpdir = tempdir()
-  
-  # extract file in temporary directory
-  system(paste0('7z x ', ifile, ' -o', tmpdir, ' -aoa'), ignore.stdout = TRUE)
-  
-  # path to extracted file
-  zfile = paste0(tmpdir, '/commondata/compass_ocsaerial.gdb/')
+  ifile = olist[[ii]]
   
   # read in observations
-  obs = st_read(zfile, layer = "Observation", quiet = TRUE) %>%
-    as.data.frame()
+  obs = read_csv(ifile, show_col_types = FALSE)
   
   # format observations
   obs = obs %>%
     transmute(
-      time = as.POSIXct(as.character(DateTime), tz = 'UTC'),
+      time = as.POSIXct(as.character(DateTime), format = "%m/%d/%Y %H:%M", tz = 'UTC'),
       date = as.Date(time),
       year = year(date),
       yday = yday(date),
@@ -58,7 +48,8 @@ for(ii in seq_along(OBS)){
       id = paste0(date, '_', platform, '_', name),
       source = 'WhaleMap'
     ) %>%
-    config_observations()
+    config_observations() %>%
+    as.data.frame()
   
   # fix species codes
   obs$species = factor(obs$species, 
@@ -71,51 +62,41 @@ for(ii in seq_along(OBS)){
   
   # store obs
   OBS[[ii]] = obs
+}
+
+for(ii in seq_along(EFF)){
+  
+  # define ifile
+  ifile = elist[[ii]]
   
   # read in effort
-  eff = st_read(zfile, layer = "PlatformTrackPoint", quiet = TRUE) %>%
-    as.data.frame()
+  eff = read_csv(ifile, show_col_types = FALSE)
   
-  # unique dates
-  eff$date = as.Date(eff$DateTime)
-  dts = unique(eff$date)
-  
-  SEG = vector('list', length(dts))
-  for(jj in seq_along(dts)){
-    
-    SEG[[jj]] = eff %>% 
-      filter(date == dts[jj]) %>%
-      transmute(
-        time = as.POSIXct(as.character(DateTime), tz = 'UTC'),
-        date = as.Date(time),
-        year = year(date),
-        yday = yday(date),
-        lat = LatPlatform,
-        lon = LongPlatform,
-        speed = NA,
-        altitude = NA,
-        platform = 'plane',
-        name = 'hdr',
-        id = paste0(date, '_', platform, '_', name),
-        source = 'WhaleMap'
-      ) %>%
-      arrange(time) %>%
-      config_tracks() %>%
-      subsample_gps()
-    
-  }
-  
-  # collapse
-  eff = bind_rows(SEG)
+  # format
+  eff = eff %>%
+    transmute(
+      time = as.POSIXct(as.character(DateTime), format = "%m/%d/%Y %H:%M", tz = 'UTC'),
+      date = as.Date(time),
+      year = year(date),
+      yday = yday(date),
+      lat = LatPlatform,
+      lon = LongPlatform,
+      speed = NA,
+      altitude = NA,
+      platform = 'plane',
+      name = 'hdr',
+      id = paste0(date, '_', platform, '_', name),
+      source = 'WhaleMap'
+    ) %>%
+    arrange(time) %>%
+    config_tracks() %>%
+    subsample_gps()
   
   # drop NA
   eff = eff %>% filter(!is.na(time) & !is.na(lat) & !is.na(lon))
   
   # store eff
   EFF[[ii]] = eff
-  
-  # remove temporary directory
-  # system(paste0("rm -r ", tmpdir))
 }
 
 # collapse data
