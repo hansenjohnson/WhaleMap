ec51281d340cfa232e7e6e42dd1d7086a6d4bfd3
Hansen Johnson <hjohnson@neaq.org>
2026-01-20 09:34:16 -0500

    DFO archived 2025, so send only current year

--- R/share_wi.R
+++ R/share_wi.R
@@ -22,8 +22,7 @@ remove_dfo_ids = function(obs){
 # process -----------------------------------------------------------------
 
 # define start time
-# t0 = as.Date(paste0(year(Sys.Date()),'-01-01'))
-t0 = as.Date('2025-01-01')
+t0 = as.Date(paste0(year(Sys.Date()),'-01-01'))
 
 # read and subset tracks
 trk = readRDS('data/processed/effort.rds') %>%
